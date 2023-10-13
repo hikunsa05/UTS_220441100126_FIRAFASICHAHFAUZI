e6e23fb44a9fe732d484a489bf10d35868d17bb8
hikunsa05 <113221885+hikunsa05@users.noreply.github.com>
2023-10-13 08:19:57 +0700

    Add files via upload

---
+++ UTS_220441100126_FIRA FASICHAH FAUZI/src/uts_220441100126_fira/fasichah/fauzi/UTS_220441100126_FIRAFASICHAHFAUZI.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package uts_220441100126_fira.fasichah.fauzi;
+
+/**
+ *
+ * @author ASUS
+ */
+public class UTS_220441100126_FIRAFASICHAHFAUZI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
